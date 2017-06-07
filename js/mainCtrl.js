5315817752270257b6133d0575d3139a6e83017e
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-06-06 20:21:33 -0600

    finished, now styling

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,18 @@
-angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+angular.module('chatroom').controller('mainCtrl', function($scope, messageService) {
+
+  $scope.getMessages = function () {
+    messageService.getMessages().then(function(response) {
+      $scope.messages = response.data
+    });
+
+}
+
+  $scope.getMessages();
+
+  $scope.postMessage = function (newMessage) {
+    messageService.postMessage (newMessage)
+  }
+
 
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
@@ -12,11 +26,10 @@ angular.module('chatroom').controller('mainCtrl', function($scope, messageServic
 
 
 
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+  // uncomment this code when your getMessages function is finished
+  // This goes and gets new data every second, which mimicking a chat room experience.
+   setInterval(function(){
+    $scope.getMessages();
+  }, 1500)
 
 })
